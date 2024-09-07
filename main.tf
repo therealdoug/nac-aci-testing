b6ffca5399afe7a03628070b5dac00f6ba76b281
Doug <34190591+therealdoug@users.noreply.github.com>
2024-09-07 13:03:08 +0000

    add data files

--- main.tf
+++ main.tf
@@ -7,18 +7,19 @@ terraform {
 }
 
 provider "aci" {
-  username = "username"
-  password = "password"
-  url      = "https://apic.url"
+  username = "admin"
+  password = "!v3G@!4@Y"
+  url      = "https://sandboxapicdc.cisco.com"
 }
 
 module "aci" {
-  source  = "netascode/nac-aci/aci"
-  version = "0.9.0"
+  #source  = "netascode/nac-aci/aci"
+  source = "github.com/therealdoug/terraform-aci-nac-aci?ref=epg_subj_labels"
+  #version = ">= 0.9.0"
 
   yaml_directories = ["data"]
 
-  manage_access_policies    = false
+  manage_access_policies    = true
   manage_fabric_policies    = false
   manage_pod_policies       = false
   manage_node_policies      = false
